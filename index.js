fd7458607ce4e08e4a0c6d6571941c0cde051054
PP Nuttapan <88316947+Rapizz1112@users.noreply.github.com>
2023-04-19 16:54:05 +0700

    Fix

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+const port = 11100;
+const io = require('socket.io')();
+io.use((socket, next) => {
+    if (socket.handshake.query.token === "UNITY") {
+        next();
+    } else {
+        next(new Error("Authentication error"));
+    }
 });
 
-var userId = 0;
-io.on('connection', function(socket){
-  socket.userId = userId ++;
-  console.log('a user connected, user id: ' + socket.userId);
+io.on('connection', socket => {
+  socket.emit('connection', {date: new Date().getTime(), data: "Hello Unity"})
 
-  socket.on('chat', function(msg){
-    console.log('message from user#' + socket.userId + ": " + msg);
-    io.emit('chat', {
-      id: socket.userId,
-      msg: msg
-    });
+  socket.on('hello', (data) => {
+    socket.emit('hello', {date: new Date().getTime(), data: data});
+  });
+
+  socket.on('spin', (data) => {
+    socket.emit('spin', {date: new Date().getTime()});
+  });
+
+  socket.on('class', (data) => {
+    socket.emit('class', {date: new Date().getTime(), data: data});
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+io.listen(port);
+console.log('listening on *:' + port);
